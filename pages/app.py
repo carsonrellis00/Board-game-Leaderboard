7105dc5e7dab0f76159939679e07313a5008f68a
carsonrellis00 <carsonrellis00@gmail.com>
2025-09-29 15:42:29 -0400

    Add files via upload

---
+++ pages/app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="🎲 Board Game Leaderboard",
+    page_icon="🎲",
+)
+
+st.title("🎲 Welcome to the Board Game Leaderboard!")
+st.write("Use the sidebar to navigate between pages.")
