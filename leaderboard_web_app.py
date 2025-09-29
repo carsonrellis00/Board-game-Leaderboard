48e2abfa1ca8cb2c928ae0382287230afc150ed4
carsonrellis00 <carsonrellis00@gmail.com>
2025-09-29 16:51:47 -0400

    Update leaderboard_web_app.py

--- leaderboard_web_app.py
+++ leaderboard_web_app.py
@@ -32,8 +32,8 @@ st.markdown("---")
 st.header("App Pages & Features")
 st.write("Welcome! Use the sidebar to navigate:")
 st.markdown("""
-- **Manage Players**: Add or remove players globally. All pages reference this list.
-- **Manual Game Entry**: Record individual or team-based games for any selected game.
+- **👥Manage Players**: Add or remove players globally. All pages reference this list.
+- **✏️Manual Game Entry**: Record individual or team-based games for any selected game.
 - **🏆Leaderboard**: View rankings for each game based on TrueSkill ratings.
 - **📜Match History**: See past matches with timestamps and results.
 - **⚔️Matchmaking**: Generate balanced teams and record team match results easily.
